724389537c97f79bb9c090d8852f05ff8e396697
Jamz <big.meel@zoho.com>
2022-09-15 03:36:12 -0500

    write tests for footer links

--- cypress/e2e/swag_labs/sauce_demo_footer.cy.js
+++ cypress/e2e/swag_labs/sauce_demo_footer.cy.js
@@ -7,9 +7,38 @@ describe('saucedemo ecommerce footer', () => {
       cy.validLogin()
     })
 
+
     it('connects to twitter page', () => {
-      cy.get('.social_twitter>a')
-        .click()
+    
+      cy.get('.social_twitter>a').should($a => {
+        expect($a.attr('href'), 'href').to.equal('https://twitter.com/saucelabs')
+        $a.attr('target', '_self')
+      }).click()
+
+      cy.location('pathname').should('include', '/saucelabs')
     })
+
+
+    it('connects to facebook page', () => {
+    
+      cy.get('.social_facebook>a').should($a => {
+        expect($a.attr('href'), 'href').to.equal('https://www.facebook.com/saucelabs')
+        $a.attr('target', '_self')
+      }).click()
+
+      cy.location('pathname').should('include', '/saucelabs')
+    })
+
+
+    it('connects to facebook page', () => {
+    
+      cy.get('.social_linkedin>a').should($a => {
+        expect($a.attr('href'), 'href').to.equal('https://www.linkedin.com/company/sauce-labs/')
+        $a.attr('target', '_self')
+      }).click()
+
+      cy.location('pathname').should('include', '/sauce-labs')
+    })
+
   })
 })
